ad4f190cb6eb2a1d4177592e896eb41e8b512afe
NgaNguyenDuy <kyo1508@gmail.com>
2015-01-12 12:52:02 +0700

    refactoring middleware

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var app = express();
+var http = require('http');
+var async = require('async');
 var AppBuilder = require('./modules/AppBuilder');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 
 // Defined some global variables
 global.__appBaseDir = __dirname;
-
 global.__appEnv = process.env.NODE_ENV || "development";
 
+
+function parallel(mid) {
+    return function(req, res, next) {
+        async.each(mid, function(mw, cb) {
+            mw(req, res, cb);
+        }, next);
+    };
+}
 
 //Initialize the config. Now the configurations will be available in _config global getter.
 AppBuilder.initConfig({
@@ -26,11 +38,46 @@ AppBuilder.initLogger(function (message, level) {
         console.log(message.fullyFormattedMessage);
     }
 });
+
+
+//
+// Config middlewares expressjs
+// ----------------------------
+
+app.use(parallel([
+    bodyParser.json(),
+    bodyParser({
+        extended: true
+    }),
+    cookieParser(_config.hashKeySecret),
+    session({
+        secret: 'yololo',
+        saveUninitialized: true,
+        resave: true
+    })
+]));
+
+
+
+//Export the app via getter in global
+global.__defineGetter__("_app", function () {
+    return app;
+});
+
+
+
+// Setup expressjs
+app.set('port', _config.app.port);
+
 app.get('/', function(req, res) {
     res.json('Yo!');
 });
 
+var _server = http.createServer(app);
+var server = _server.listen(app.get('port'), function() {
+    log.info('Server was running at ' + _config.app.serverUrl);
+});
 
-app.listen('8888', function() {
-    log.info('Listen on port 8888');
+server.on('error', function(err) {
+    log.error(err);
 });
