1495f5e014694a9228b839d1083ee7418f45fc73
NgaNguyenDuy <kyo1508@gmail.com>
2015-03-13 22:18:14 +0700

    added mongodb database (cherry picked from commit 31c3c46)

---
+++ models/User.js
@@ -0,0 +1,22 @@
+
+exports.schema = {
+    email: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    tokenId: {
+        type: String
+    }
+};
+
+exports.static = {
+
+};
--- modules/AppBuilder/libs/main.js
+++ modules/AppBuilder/libs/main.js
@@ -2,6 +2,7 @@ var ConfigManager = require("./modules/ConfigManager");
 var Logger = require("./modules/Logger");
 var fs = require("fs");
 var path = require("path");
+var MongoDatabaseProvider = require("./modules/MongoDBProvider");
 require("colors");
 
 
@@ -19,4 +20,34 @@ exports.initConfig = function (options) {
 //Initialize teh logger for the application. Consumer is teh method which will consume the produces logs.
 exports.initLogger = function (consumer) {
     global.log = new Logger(consumer, _config.logger);
+};
+
+
+//Init all database Models
+exports.initModels = function (callback) {
+    MongoDatabaseProvider.getDatabase(function (db) {
+        Object.defineProperty(global, '_db', {
+            get: function () {
+                return db;
+            }
+        });
+        
+        fs.readdir(path.join(__appBaseDir, "models"), function (err, list) {
+            if (err) log.error(err);
+            else {
+                
+                list.forEach(function (item) {
+                    var name = item.toString().replace(/\.js$/, "");
+                    var model = db.getModel(name);
+                    model.ensureAllManuallyDefinedSchemaIndexes();
+                    Object.defineProperty(global, name, {
+                        get: function () {
+                            return model;
+                        }
+                    });
+                });
+            }
+            callback();
+        });
+    });
 };
