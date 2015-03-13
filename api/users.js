a9be3644bb1c1a1c49a13534fc2240a370175de3
NgaNguyenDuy <kyo1508@gmail.com>
2015-03-13 22:40:31 +0700

    change something in server (cherry picked from commit 1e68b70) (cherry picked from commit bd0b6e9)

--- api/users.js
+++ api/users.js
@@ -14,42 +14,6 @@ router.use(function timeLog(req, res, next) {
 
 // define the home page route
 router.get('/', function(req, res) {
-    console.time("put");
-    var key = db.as.key('test', "tables");
-    var bins = {
-        i: 1233,
-        s: "abc",
-        l: [1, 2, 3],
-        m: { s: "g3", i: 3, b: new Buffer( [0xa, 0xb, 0xc])},
-        b: new Buffer([0xa, 0xb, 0xc]),
-        b2: new Uint8Array([0xa, 0xb, 0xc])
-    };
-    var metadata = {
-        ttl: 10000,
-        gen: 0
-    };
-    db.client.put(key, bins, metadata, function(err, key) {
-
-        var exitCode = 0;
-
-        switch ( err.code ) {
-            case db.as.status.AEROSPIKE_OK:
-                break;
-
-            default:
-                console.error("Error: " + err.message);
-                exitCode = 1;
-                break;
-        }
-
-
-            console.log("--->", key);
-            console.timeEnd("put");
-
-
-       // process.exit(exitCode);
-    });
-
     res.send('DATA:', key);
 });
 
